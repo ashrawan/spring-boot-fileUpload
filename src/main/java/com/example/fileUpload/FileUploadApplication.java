f4a0a13a5e9e95e002664e40ff9891f11ea845e9
mausamRayamajhi <mausam495@gmail.com>
2018-08-22 21:48:55 +0545

    commentAdded

--- src/main/java/com/example/fileUpload/FileUploadApplication.java
+++ src/main/java/com/example/fileUpload/FileUploadApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// this is spring boot application
 public class FileUploadApplication {
-
+// this is main
 	public static void main(String[] args) {
 		SpringApplication.run(FileUploadApplication.class, args); 
 	}
